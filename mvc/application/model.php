6b50f9b28dfeef17e1ad93195a80621ea4e6c673
claudiajaramillo85 <claudiajaramillo85@gmail.com>
2018-09-04 20:58:41 -0500

    Actualizacion Repositorio Septiembre 4

---
+++ mvc/application/model.php
@@ -0,0 +1,13 @@
+<?php 
+class Model {
+	public function getInfo(){
+		return array (
+			'firstName'   =>'Jeremias',
+			'lastName'    =>'Springfield',
+			'email'       => 'jeremias@gmail.com',
+			'phoneNumber' => 3120909900
+		);
+	}
+	
+}
+?>
